a38acb24ee8bcc2be2af261a1d40c3f7366f7f26
Sara Oros <94159081+saraoros@users.noreply.github.com>
2022-08-04 20:53:46 -0700

    small change to js file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const celsius = kelvin - 273; // Celsius is now 20
 console.log("Celsiuis converted from kelvin: ", celsius);
 
 // This variable, Fahrenheit, is converting the previous celsius variable by dividing 9 by 5, multiplying the celsius variable value then adding 32 to it.
+
 let fahrenheit = celsius * (9 / 5) + 32;
 console.log("Fahrenheit result: ", fahrenheit);
 
